4708426008ee83271c21e00723bd4840d5eddbf7
Herman Schoenfeld <herman@sphere10.com>
2022-10-17 11:37:37 +1000

    Moved CMSProperties to LocalNotionEditableResource for coming DB support

--- LocalNotion.Core/DataObjects/LocalNotionEditableResource.cs
+++ LocalNotion.Core/DataObjects/LocalNotionEditableResource.cs
@@ -7,6 +7,11 @@ public abstract class LocalNotionEditableResource : LocalNotionResource {
 	[JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
 	public string Name { get; set; }
 
+
+	[JsonProperty("cms", NullValueHandling = NullValueHandling.Ignore)]
+	public CMSProperties CMSProperties { get; set; } = null;
+
+
 	[JsonProperty("last_edited_time")]
 	public DateTime LastEditedTime { get; set; }
 }
