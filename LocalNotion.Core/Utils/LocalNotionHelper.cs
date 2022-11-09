93a959495168c953fb92750d6a9f144d5aa00ff3
Herman Schoenfeld <herman@sphere10.com>
2022-11-09 23:55:56 +1000

    Bug fix with page name

--- LocalNotion.Core/Utils/LocalNotionHelper.cs
+++ LocalNotion.Core/Utils/LocalNotionHelper.cs
@@ -51,6 +51,7 @@ internal class LocalNotionHelper {
 		dest.LastEditedOn = page.LastEditedTime;
 		dest.CreatedOn = page.CreatedTime;
 		dest.Title = page.GetTitle().ToValueWhenNullOrEmpty(Constants.DefaultResourceTitle);
+		dest.Name = CalculatePageName(page.Id, dest.Title); // note: this is made unique by NotionSyncOrchestrator
 		dest.Cover = page.Cover != null ? ParseFileUrl(page.Cover, out _) : null;
 		if (page.Icon != null) {
 			dest.Thumbnail = new() {
@@ -136,6 +137,9 @@ internal class LocalNotionHelper {
 		return result;
 	}
 
+	
+	public static string CalculatePageName(string id, string title) 
+		=> Tools.Url.ToHtml4DOMObjectID($"{Tools.Text.ToCasing(TextCasing.KebabCase, title)}", String.Empty);
 
 
 }
