3cf3010a0fb525911c7005659f32262504fb1e5e
Herman Schoenfeld <herman@sphere10.com>
2022-12-02 18:02:24 +1000

    Fix weird cosmetic issue

--- LocalNotion.Core/Themes/default/resources/local-notion/js/ln.js
+++ LocalNotion.Core/Themes/default/resources/local-notion/js/ln.js
@@ -2,5 +2,10 @@ $(document).ready(function ($) {
 
     // remove empty block children
     $(".ln-block-children:not(:has(*))").remove();
+    document.querySelectorAll('.ln-block-children').forEach(elem => {
+        var $elem = $(elem);
+        if ($elem.html().trim().length === 0)
+            $elem.remove();
+    });
     
 });
