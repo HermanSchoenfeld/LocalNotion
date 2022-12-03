a0aa145cb374f306d236e7a6e7289156e0ef0203
Herman Schoenfeld <herman@sphere10.com>
2022-12-03 13:35:00 +1000

    Fix footer theme cosmetic issues

--- LocalNotion.Core/Themes/default/resources/local-notion/js/ln.js
+++ LocalNotion.Core/Themes/default/resources/local-notion/js/ln.js
@@ -1,11 +1,4 @@
-$(document).ready(function ($) {
-
+$(document).ready(function () {
     // remove empty block children
     $(".ln-block-children:not(:has(*))").remove();
-    document.querySelectorAll('.ln-block-children').forEach(elem => {
-        var $elem = $(elem);
-        if ($elem.html().trim().length === 0)
-            $elem.remove();
-    });
-    
 });
