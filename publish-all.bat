05e3de0021e5329e6c4e6939d11990d415b8455b
Herman Schoenfeld <herman@sphere10.com>
2024-10-31 03:47:21 +1000

    Build env update

--- publish-all.bat
+++ publish-all.bat
@@ -4,40 +4,40 @@ dotnet build -c Release
 if %ERRORLEVEL% NEQ 0 goto Exit
 
 dotnet publish -c Release /p:PublishProfile=win-x64
-del Z:\Builds\LocalNotion\latest\win-x64\*.pdb
-del Z:\Builds\LocalNotion\latest\win-x64\*.dll
+del y:\builds\LocalNotion\latest\win-x64\*.pdb
+del y:\builds\LocalNotion\latest\win-x64\*.dll
 
 dotnet publish -c Release /p:PublishProfile=win-x86
-del Z:\Builds\LocalNotion\latest\win-x86\*.pdb
-del Z:\Builds\LocalNotion\latest\win-x86\*.dll
+del y:\builds\LocalNotion\latest\win-x86\*.pdb
+del y:\builds\LocalNotion\latest\win-x86\*.dll
 
 dotnet publish -c Release /p:PublishProfile=win-arm64
-del Z:\Builds\LocalNotion\latest\win-arm64\*.pdb
-del Z:\Builds\LocalNotion\latest\win-arm64\*.dll
+del y:\builds\LocalNotion\latest\win-arm64\*.pdb
+del y:\builds\LocalNotion\latest\win-arm64\*.dll
 
 dotnet publish -c Release /p:PublishProfile=win-arm
-del Z:\Builds\LocalNotion\latest\win-arm\*.pdb
-del Z:\Builds\LocalNotion\latest\win-arm\*.dll
+del y:\builds\LocalNotion\latest\win-arm\*.pdb
+del y:\builds\LocalNotion\latest\win-arm\*.dll
 
 dotnet publish -c Release /p:PublishProfile=osx-x64
-del Z:\Builds\LocalNotion\latest\osx-x64\*.pdb
-del Z:\Builds\LocalNotion\latest\osx-x64\*.dll
+del y:\builds\LocalNotion\latest\osx-x64\*.pdb
+del y:\builds\LocalNotion\latest\osx-x64\*.dll
 
 dotnet publish -c Release /p:PublishProfile=osx-arm64
-del Z:\Builds\LocalNotion\latest\osx-arm64\*.pdb
-del Z:\Builds\LocalNotion\latest\osx-arm64\*.dll
+del y:\builds\LocalNotion\latest\osx-arm64\*.pdb
+del y:\builds\LocalNotion\latest\osx-arm64\*.dll
 
 dotnet publish -c Release /p:PublishProfile=linux-x64
-del Z:\Builds\LocalNotion\latest\linux-x64\*.pdb
-del Z:\Builds\LocalNotion\latest\linux-x64\*.dll
+del y:\builds\LocalNotion\latest\linux-x64\*.pdb
+del y:\builds\LocalNotion\latest\linux-x64\*.dll
 
 dotnet publish -c Release /p:PublishProfile=linux-arm64
-del Z:\Builds\LocalNotion\latest\linux-arm64\*.pdb
-del Z:\Builds\LocalNotion\latest\linux-arm64\*.dll
+del y:\builds\LocalNotion\latest\linux-arm64\*.pdb
+del y:\builds\LocalNotion\latest\linux-arm64\*.dll
 
 dotnet publish -c Release /p:PublishProfile=linux-arm
-del Z:\Builds\LocalNotion\latest\linux-arm\*.pdb
-del Z:\Builds\LocalNotion\latest\linux-arm\*.dll
+del y:\builds\LocalNotion\latest\linux-arm\*.pdb
+del y:\builds\LocalNotion\latest\linux-arm\*.dll
 
 :Exit
 pause
--- publish-linux-x64.bat
+++ publish-linux-x64.bat
@@ -4,8 +4,8 @@ dotnet build -c Release
 if %ERRORLEVEL% NEQ 0 goto Exit
 
 dotnet publish -c Release /p:PublishProfile=linux-x64
-del Z:\Builds\LocalNotion\latest\linux-x64\*.pdb
-del Z:\Builds\LocalNotion\latest\linux-x64\*.dll
+del y:\builds\LocalNotion\latest\linux-x64\*.pdb
+del y:\builds\LocalNotion\latest\linux-x64\*.dll
 
 :Exit
 pause
